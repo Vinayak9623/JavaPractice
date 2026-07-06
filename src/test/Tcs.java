3c8a9484909ad6eaa099b8edc7b787a5c5ca1127
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-07-06 20:36:09 +0530

    Interview prepration

---
+++ src/test/Tcs.java
@@ -0,0 +1,24 @@
+package test;
+
+public class Tcs {
+
+    static void main() {
+        System.out.println("Hello World"+10+10+10);
+        System.out.println(10+10+"Hello World");
+
+        Integer i=127;
+        Integer j=127;
+        System.out.println(i==j);
+        System.out.println(i.equals(j));
+
+        Integer x=128;
+        Integer y=128;
+        System.out.println(x==y);
+       System.out.println(x.equals(y));
+
+        String s="vinayak";
+        String s1="vinayak";
+        System.out.println(s==s1);
+        System.out.println(s.equals(s1));
+    }
+}
