2c30098db124d6e72ef7056cd266403da6b91cf9
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-07-11 19:56:23 +0530

    practice

---
+++ src/test/Change.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Change {
+
+    static void main() {
+        Change change=new Change();
+        test(change);
+        System.out.println(change);
+    }
+
+    public static void test(Change change){
+    }
+}
