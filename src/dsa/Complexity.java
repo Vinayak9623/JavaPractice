d795a174fb8126e76e6b43310ec580fb7cbcbaf9
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-07-13 12:52:21 +0530

    common complexity with examples

--- src/dsa/Complexity.java
+++ src/dsa/Complexity.java
@@ -3,9 +3,23 @@ package dsa;
 public class Complexity {
     static void main() {
 
-        worstCase();
-        bestCase();
-        commonComplexity();
+        Integer x=10;//Integer.valueOf(10); AutoBoxing
+         int y=x; // x.IntValue(); unBoxing
+
+        //cache -128 to +127
+
+
+        Integer a=127;
+        Integer b=127;
+
+        int c=128;
+        int z=128;
+        System.out.println(c==z);
+        System.out.println(a==b);
+
+//        worstCase();
+//        bestCase();
+//        commonComplexity();
     }
 
     //Big O
@@ -66,5 +80,38 @@ public class Complexity {
         //8. O(n!) worst among common algorithm
 
 
+    }
+
+//O(log n)
+    public static void binaryExample(){
+        int[] arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int target=15;
+        int left=0;
+        int right=arr.length-1;
+        while (left<=right){
+
+            int middle=(left+right)/2;
+
+            if(target>arr[middle]){
+                left=middle+1;
+            }
+            else if(target<arr[middle]){
+                right=middle-1;
+            }
+            else {
+                System.out.println("Target found:"+arr[middle]);
+                break;
+            }
+        }
+
+        System.out.println("Target not found");
+    }
+
+
+    public static void linearExample(){
+     int[] arr={1,2,3,4,5,6,7,8,9,10};
+     for(int i=0;i<arr.length;i++){
+         System.out.println(arr[i]);
+     }
     }
 }
