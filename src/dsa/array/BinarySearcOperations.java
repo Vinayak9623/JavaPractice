024ded56b61c773f9fc0571a843231b05b8ac6b9
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-07-28 12:59:01 +0530

    Binary search operation

---
+++ src/dsa/array/BinarySearcOperations.java
@@ -0,0 +1,40 @@
+package dsa.array;
+
+public class BinarySearcOperations {
+    static void main() {
+        int[] arr={10,20,30,40,50,60,70,80,90,100};
+
+        int indexWithLinearSearch = findIndexWithLinearSearch(arr, 60);
+        System.out.println(indexWithLinearSearch);
+        int index = findIndex(arr, 50);
+        System.out.println(index);
+    }
+
+    public static int findIndexWithLinearSearch(int[] arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target)return i;
+        }
+        return -1;
+    }
+    public static int findIndex(int[] arr,int target){
+        int left=0;
+        int right=arr.length-1;
+        int middle=left+(right-left)/2;
+
+        while (left<right) {
+            if (arr[middle] == target) return middle;
+            if (arr[middle] > target) {
+                right=middle-1;
+                middle=left+right-left/2;
+            }
+            else {
+                left=middle+1;
+                middle=left+right-left/2;
+            }
+            }
+
+        return middle;
+        }
+
+
+}
